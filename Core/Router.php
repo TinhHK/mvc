569ef0c55d6a05092f1d3a3df49863f02b7643ef
mrktinh <mrktinh@gmail.com>
2020-10-02 07:55:01 +0700

    add routing table in Router

--- Core/Router.php
+++ Core/Router.php
@@ -2,4 +2,22 @@
 
 class Router {
 
+    // routing table
+    private $routes;
+
+    /*
+     * add route to routing table
+     * @param string $route
+     * @param array $param
+     */
+    public function add($route, $param)
+    {
+        $this->routes[$route] = $param;
+    }
+
+    // get routing table
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
 }
--- public/index.php
+++ public/index.php
@@ -1,7 +1,11 @@
 <?php
 
-require '../Core/Router.php';
+require_once '../Core/Router.php';
 
 $router = new Router();
-echo get_class($router);
 
+$router->add('/', ['Controller' => 'Home', 'Action' => 'index']);
+$router->add('posts', ['Controller' => 'Posts', 'Action' => 'index']);
+$router->add('posts/new', ['Controller' => 'Posts', 'Action' => 'new']);
+
+var_dump($router->getRoutes());
