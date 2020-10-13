22789317d8af1a4d865f411794c1ea108195d1e4
mrktinh <mrktinh@gmail.com>
2020-10-13 08:42:09 +0700

    add and match route using regex

--- Core/Router.php
+++ Core/Router.php
@@ -2,27 +2,62 @@
 
 class Router {
 
-    // params contain controller and action of route
-    private $params;
+    /* Routing table
+     * @var array
+     */
+    private $route;
+
+    /* params contain controller and action of route
+     * @var array
+     */
+    private $param;
+
+    /* convert query string to regex, then add to routing table
+     * @param $route string
+     * @param $parma array
+     * @return void
+     */
+    public function add($route, $param = [])
+    {
+        // add escape sequence to /
+        $route = preg_replace('/\//', '\/', $route);
+        // convert variable
+        $route = preg_replace('/\{([a-z]+)\}/', '(?<\1>[a-z-]+)', $route);
+        // add start and end delimiters, and case insensitive flag
+        $route = '/^'.$route.'$/i';
+        $this->route[$route] = $param;
+
+    }
 
 
+    /* match url from route to regex, if existing param, it will be overridden
+     * @param $url string
+     * @return boolean
+     */
     public function match(string $url)
     {
-        $pattern = "/^(?<controller>[a-z-]+)\/(?<action>[a-z-]+)$/";
-        if(preg_match($pattern, $url, $matches)) {
-            foreach ( $matches as $key => $val) {
-                if(is_string($key)) {
-                    $this->params[$key] = $val;
+        foreach($this->route as $route => $param) {
+            if(preg_match($route, $url, $matches)) {
+                foreach ($matches as $name => $match) {
+                    if(is_string($name)) {
+                        $param[$name] = $match;
+                    }
                 }
+                $this->param = $param;
+                return true;
             }
-            return true;
         }
         return false;
 
     }
 
+    public function getRoute()
+    {
+        return $this->route;
+    }
+
     public function getParam()
     {
-        return $this->params;
+        return $this->param;
     }
 }
--- public/index.php
+++ public/index.php
@@ -4,9 +4,11 @@ require '../Core/Router.php';
 
 $router = new Router();
 
-//$router->add('', ['Controller' => 'Home', 'Action' => 'index']);
-//$router->add('posts', ['Controller' => 'Posts', 'Action' => 'index']);
-//$router->add('posts/new', ['Controller' => 'Posts', 'Action' => 'new']);
+$router->add('', ['Controller' => 'Home', 'Action' => 'index']);
+$router->add('posts', ['Controller' => 'Posts', 'Action' => 'index']);
+$router->add('posts/new', ['Controller' => 'Posts', 'Action' => 'new']);
+$router->add('{controller}/{action}', ['controller' => 'add', 'action' => 'sad']);
+$router->add('admin/{action}/{controller}');
 
 $url = $_SERVER['QUERY_STRING'];
 
